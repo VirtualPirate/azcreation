c1b6e5feb61a0025fbda5d42e515abc480d4a5d1
VirtualPirate <artaza13020artaza@gmail.com>
2023-01-10 00:06:08 +0530

    Added animation to inro-subject-box and modified the animeation of desk-nav-react

--- components/intro-subject/intro-subject.component.tsx
+++ components/intro-subject/intro-subject.component.tsx
@@ -1,5 +1,7 @@
-import { ThemeContext } from "../../contexts/theme.context";
-import { useContext } from "react";
+import { ThemeContext, ThemeType } from "../../contexts/theme.context";
+import { useContext, useState, useEffect, useRef } from "react";
+
+import anime from "animejs/lib/anime.min.js";
 
 import styles from "./intro-subject.module.css";
 
@@ -17,19 +19,44 @@ const themeAssets = {
 
 export default function IntroSubject() {
   const { theme } = useContext(ThemeContext);
+  const [themeState, setThemeState] = useState<ThemeType>(theme);
+  const introBoxRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (theme === themeState) return;
+
+    anime({
+      targets: introBoxRef.current,
+      scale: 0,
+      opacity: 0,
+      duration: 500,
+      easing: "cubicBezier(0, 0, 0.28, 1)",
+      complete: () => {
+        setThemeState(theme);
+        anime({
+          targets: introBoxRef.current,
+          scale: 1,
+          opacity: 1,
+
+          duration: 500,
+          easing: "cubicBezier(0, 0, 0.28, 1)",
+        });
+      },
+    });
+  }, [theme]);
 
   return (
-    <div className={styles.intro_box}>
+    <div ref={introBoxRef} className={styles.intro_box}>
       <div className={`${styles.box_stroke} ${styles.box}`}></div>
       <div className={`${styles.glow_box} ${styles.box}`}></div>
 
       <img
-        src={themeAssets[theme].subjectUp}
+        src={themeAssets[themeState].subjectUp}
         alt=""
         className={`${styles.subject_up} ${styles.subject}`}
       />
       <img
-        src={themeAssets[theme].subjectDown}
+        src={themeAssets[themeState].subjectDown}
         alt=""
         className={`${styles.subject_down} ${styles.subject}`}
       />
