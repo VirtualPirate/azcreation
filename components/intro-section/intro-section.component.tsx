92d90bc3110356a538fba567445b0fde180e3fc8
VirtualPirate <artaza13020artaza@gmail.com>
2023-01-09 21:22:47 +0530

    Fixed project for building

--- components/intro-section/intro-section.component.tsx
+++ components/intro-section/intro-section.component.tsx
@@ -11,9 +11,9 @@ export default function IntroSection() {
         <h2 className={styles.intro_heading}>AZ CREATION STUDIO</h2>
         <p className={styles.intro_text}>
           I am a self taught designer. Currently a Filmmaker as a career path.
-          I've designing experience of over 4 Years. I started at the early age
-          16. Fascinated by Photography, Filemaking and Designing. Since, then I
-          have never stopped learning
+          I&#39;ve designing experience of over 4 Years. I started at the early
+          age 16. Fascinated by Photography, Filemaking and Designing. Since,
+          then I have never stopped learning
         </p>
       </div>
     </section>
--- components/skill-box/skill-box.component.tsx
+++ components/skill-box/skill-box.component.tsx
@@ -52,7 +52,7 @@ export default function SkillBox({
       delay: delay,
       easing: easing,
     });
-  }, []);
+  }, [delay]);
 
   return (
     <svg
