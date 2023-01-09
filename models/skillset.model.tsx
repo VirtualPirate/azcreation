2186aef2ed5353aa2025c7a0ddd8dcaa361168b4
VirtualPirate <artaza13020artaza@gmail.com>
2023-01-09 21:09:11 +0530

    - Added Audition, Added Items bottom animation, Optimized images

--- models/skillset.model.tsx
+++ models/skillset.model.tsx
@@ -28,7 +28,7 @@ const skills: Array<SkillType> = [
   {
     name: "ILLUSTRATOR",
     logo: "illustrator.svg",
-    skillLevel: 4,
+    skillLevel: 2,
     border_color: "#F87D0C",
     gradient_color: "#D07623",
     glow_matrix:
@@ -38,7 +38,7 @@ const skills: Array<SkillType> = [
   {
     name: "AFTER EFFECTS",
     logo: "aftereffects.svg",
-    skillLevel: 4,
+    skillLevel: 3,
     border_color: "#D192FE",
     gradient_color: "#6F23D0",
     glow_matrix: "0 0 0 0 0.819608 0 0 0 0 0.572549 0 0 0 0 0.996078 0 0 0 1 0",
@@ -56,10 +56,20 @@ const skills: Array<SkillType> = [
   {
     name: "ANIMATE",
     logo: "animate.svg",
-    skillLevel: 4,
+    skillLevel: 3,
     border_color: "#FF4A1D",
     gradient_color: "#D02D23",
     glow_matrix: "0 0 0 0 1 0 0 0 0 0.290196 0 0 0 0 0.113725 0 0 0 1 0",
+  },
+
+  {
+    name: "AUDITION",
+    logo: "audition.svg",
+    skillLevel: 3,
+    border_color: "#03E2BB",
+    gradient_color: "#03E2BB",
+    glow_matrix:
+      "0 0 0 0 0.0117647 0 0 0 0 0.886275 0 0 0 0 0.733333 0 0 0 1 0",
   },
 
   {
@@ -74,7 +84,7 @@ const skills: Array<SkillType> = [
   {
     name: "MAYA",
     logo: "maya.png",
-    skillLevel: 4,
+    skillLevel: 3,
     border_color: "#33BBCB",
     gradient_color: "#23C6D0",
     glow_matrix: "0 0 0 0 0.2 0 0 0 0 0.733333 0 0 0 0 0.796078 0 0 0 1 0",
@@ -83,7 +93,7 @@ const skills: Array<SkillType> = [
   {
     name: "Z BRUSH",
     logo: "zbrush.png",
-    skillLevel: 4,
+    skillLevel: 1,
     border_color: "#FBFBFB",
     gradient_color: "#FFFFFF",
     glow_matrix: "0 0 0 0 0.984314 0 0 0 0 0.984314 0 0 0 0 0.984314 0 0 0 1 0",
