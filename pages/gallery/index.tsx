176c18be00446ade6ed8c21d5d3d7bc927139bd9
VirtualPirate <artaza13020artaza@gmail.com>
2023-02-22 10:49:25 +0530

    Created the gallery component

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -1,60 +1,14 @@
 import Head from "next/head";
 
-import GalleryCarousel from "../../components/gallery-carousel/gallery-carousel.component";
-import { GalleryDialogContext } from "../../contexts/gallery-dialog.context";
-import { useState, useRef, MouseEvent, useContext, useEffect } from "react";
-
+import Gallery from "../../components/gallery/gallery.component";
 import DeskNav from "../../components/desk-nav/desk-nav.component";
-
-import styles from "./gallery.module.css";
-
 import fs from "fs";
 
 type GalleryProps = {
   images: Array<string>;
 };
 
-export default function Gallery({ images }: GalleryProps) {
-  const [imageIndex, setImageIndex] = useState(0);
-  const { modalState, setModalState } = useContext(GalleryDialogContext);
-
-  const galleryDialog = useRef<HTMLDialogElement | null>(null);
-
-  useEffect(() => {
-    setModalState(false);
-  }, []);
-
-  useEffect(() => {
-    if (modalState && !galleryDialog.current?.open)
-      // if modal is open
-      galleryDialog.current?.showModal();
-    else {
-      galleryDialog.current?.close();
-    }
-  }, [modalState]);
-
-  function openModal() {
-    setModalState(true);
-    // galleryDialog.current?.showModal();
-  }
-
-  function closeModal() {
-    setModalState(false);
-    // galleryDialog.current?.close();
-  }
-
-  function dialogClickEvent(event: MouseEvent<HTMLDialogElement>) {
-    const rect = galleryDialog.current?.getBoundingClientRect();
-
-    const clickedInDialog =
-      rect!.top <= event.clientY &&
-      event.clientY <= rect!.top + rect!.height &&
-      rect!.left <= event.clientX &&
-      event.clientX <= rect!.left + rect!.width;
-
-    if (clickedInDialog === false) closeModal();
-  }
-
+export default function PhotoGallery({ images }: GalleryProps) {
   return (
     <>
       <Head>
@@ -63,44 +17,7 @@ export default function Gallery({ images }: GalleryProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <DeskNav />
-      <h1 className={styles.h1}> MANIPULATIONS </h1>
-      <div>
-        {/* {modalIsOpen ? (
-          <div className={styles.close_button} onClick={closeModal}>
-            &#10006;
-          </div>
-        ) : (
-          ""
-        )} */}
-
-        <section className={styles.gallery_section}>
-          <div className={`${styles.photo_editing_gallery} ${styles.gallery}`}>
-            {/* <h1 className={styles.h1}>MANIPULATIONS</h1> */}
-            <div className={styles.gallery_items}>
-              {images.map((image, index) => (
-                <div
-                  className={styles.gallery_item}
-                  onClick={() => {
-                    setImageIndex(index);
-                    openModal();
-                  }}
-                  key={index}
-                >
-                  <img src={image} alt="" />
-                </div>
-              ))}
-            </div>
-          </div>
-        </section>
-
-        <dialog
-          ref={galleryDialog}
-          className={styles.gallery_dialog}
-          onClick={dialogClickEvent}
-        >
-          <GalleryCarousel images={images} index={imageIndex} />
-        </dialog>
-      </div>
+      <Gallery title="MANIPULATIONS" images={images} />
     </>
   );
 }
